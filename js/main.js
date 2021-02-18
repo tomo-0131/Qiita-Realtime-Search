924a60abab88b87e5e886a1dab3410b039a7b217
tomo-0131 <woovoodoor@yahoo.co.jp>
2021-02-18 20:57:08 +0900

    First Commit

---
+++ js/main.js
@@ -0,0 +1,45 @@
+const app = new Vue ({
+  el: '#app',
+  vuetify: new Vuetify(),
+  data: {
+    items: null,
+    keyword: '',
+    message: ''
+  },
+  // 監視 インプット内の文字変化を監視する
+  watch: {
+    keyword: function(newKeyword, oldKeyword) {
+      //console.log(newKeyword)
+      this.message ='Waiting for you to stop styping...'
+      this.debouncedGetAnswer()
+    }
+  },
+  created: function(){
+    //this.keyword = 'JavaScript'
+    //this.getAnswer()
+    this.debouncedGetAnswer = _.debounce(this.getAnswer, 1000)
+  },
+  methods: {
+    getAnswer: function() {
+      if(this.keyword === '') {
+        this.items = null
+        this.message = ''
+        return
+      }
+      this.message = 'Loading...'
+      let vm = this
+      let params = { page: 1, per_page: 20, query: this.keyword }
+      axios.get('https://qiita.com/api/v2/items', { params })
+        .then(function(response){
+          console.log(response)
+          vm.items = response.data
+        })
+        .catch(function(error){
+          vm.message = 'Error!' + error
+        })
+        .finally(function() {
+          vm.message = ''
+        })
+    }
+  }
+})
